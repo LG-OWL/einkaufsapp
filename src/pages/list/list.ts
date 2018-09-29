143756d11ba649dad90ca8f042b7ad4351d51b30
Jonas <37225007+jonas-ITruns@users.noreply.github.com>
2018-09-29 14:07:08 +0200

    Item hinzufügen
    Item zur Datenbank hinzufügen oder entfernen

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'page-list',
@@ -9,30 +11,56 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 })
 export class ListPage {
   selectedItem: any;
-  items: Array<{title: string, amount: string}>;
-  itemNumber: number;
-  itemList: AngularFireList<any>;
+  //items: Array<{title: string, amount: string}>;
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
+
+  groupname: string;
+  listname: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public afDatabase: AngularFireDatabase) {
-    this.itemList = afDatabase.list('/list');
-    this.itemList.snapshotChanges().subscribe(actions => { 
-      this.items = [];
-      actions.forEach(action => {
-        const title = action.key;
-        const amount = action.payload.toJSON();
-        this.items.push({
-          title: title,
-          amount: amount.toString()
-        })
-      });
-    });
+    this.groupname = "group 1";
+    this.listname = "list 1";
+    this.itemsRef = afDatabase.list('/groups/' + this.groupname + '/' + this.listname);
+    
+    this.items = this.itemsRef.snapshotChanges().pipe(
+      map(changes => 
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
 
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
   }
 
-  itemTapped(event, item) {
-    
+  addItem(newName: string, newAmount: string) {
+    this.itemsRef.push({ name: newName, amount: newAmount });
   }
+  updateItem(key: string, newName: string, newAmount: string) {
+    this.itemsRef.update(key, { name: newName, amount: newAmount });
+  }
+  deleteItem(key: string) {
+    this.itemsRef.remove(key);
+  }
+  deleteEverything() {
+    this.itemsRef.remove();
+  }
+
 }
+
+
+//<ion-content>
+//    <ul>
+//      <li *ngFor="let item of items | async">
+//        <input type="text" #updatename [value]="item.name" />
+//        <input type="text" #updateamount [value]="item.amount" />
+//       <button (click)="updateItem(item.key, updatename.value, updateamount.value)">Update</button>
+//        <button (click)="deleteItem(item.key)">Delete</button>
+//      </li>
+//    </ul>
+//    <input type="text" #newitemname />
+//    <input type="text" #newitemamount />
+//    <button (click)="addItem(newitemname.value, newitemamount.value)">Add</button>
+//    <button (click)="deleteEverything()">Delete All</button>
+//</ion-content>
