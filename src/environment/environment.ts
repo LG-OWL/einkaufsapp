7c229eb5ea6ddf4c24945e867668f0cc3dbc885b
Jonas <37225007+jonas-ITruns@users.noreply.github.com>
2018-09-30 10:58:49 +0200

    environment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyBxgod9VNi4OHv1CegI6yrVOTHjGzhjhC0",
-        authDomain: "einkaufsapp-c50ea.firebaseapp.com",
-        databaseURL: "https://einkaufsapp-c50ea.firebaseio.com",
-        projectId: "einkaufsapp-c50ea",
-        storageBucket: "einkaufsapp-c50ea.appspot.com",
-        messagingSenderId: "332921836730"
+        apiKey: "api",
+        authDomain: "domain",
+        databaseURL: "url",
+        projectId: "id",
+        storageBucket: "bucket",
+        messagingSenderId: "senderid"
     }
 };
