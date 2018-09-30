2c9f40b2123f1ef16386e2c01170e996efa8c241
LG-OWL <CallMeEnte@web.de>
2018-09-30 11:06:39 +0200

    app module einkaufbarcode bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,6 @@ import { RegisterPage } from '../pages/register/register';
 import { GroupsPage } from '../pages/groups/groups';
 import { NewItemPage } from '../pages/new-item/new-item';
 import {EinkaufbarcodePage} from '../pages/einkaufbarcode/einkaufbarcode';
-=======
-import { EinkaufbarcodePage } from '../pages/einkaufbarcode/einkaufbarcode';
->>>>>>> e5b8af482bd219ffe5e2b1154532baf585917409
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
