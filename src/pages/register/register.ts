2e06fe723061ad826501f4edcc0f24011a9d4588
Jan <37225011+jan-ITruns@users.noreply.github.com>
2018-09-30 11:26:28 +0200

    2

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from '../../Models/user';
 import { AngularFireAuth } from "angularfire2/auth";
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { LoginPage } from '../login/login';
+import { GroupsPage } from '../groups/groups';
 
 @IonicPage()
 @Component({
@@ -12,8 +14,9 @@ import { LoginPage } from '../login/login';
 export class RegisterPage {
 
   user = {} as User;
+  userdataRef: AngularFireList<any>;
 
-  constructor(private afAuth: AngularFireAuth,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private afAuth: AngularFireAuth,public navCtrl: NavController, public navParams: NavParams, public afDatabase: AngularFireDatabase) {
   }
 
   ionViewDidLoad() {
@@ -23,6 +26,11 @@ export class RegisterPage {
   async register(user: User){
     // registrieren
     const result = await this.afAuth.auth.createUserWithEmailAndPassword(user.email,user.password);
+
+    GroupsPage.email = user.email.replace(".", "|");
+    this.userdataRef = this.afDatabase.list('/users/' + GroupsPage.email + '/data/' + user.username);
+    this.userdataRef.push({ username: user.username });
+
     this.navCtrl.push(LoginPage);
   }
 }
